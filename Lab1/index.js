57c05439c5ed271c2f17d4e57ccb03fca510a083
YakymivDanylo <dr.yakimiv@gmail.com>
2025-02-22 14:46:56 +0200

    changed textContent to innerHTML

--- Lab1/index.js
+++ Lab1/index.js
@@ -1,21 +1,21 @@
 let title = document.querySelector("h1");
 
 title.addEventListener("mouseover",function () {
-    title.textContent="Hello world!"
+    title.innerHTML="Hello world!"
 });
 
 title.addEventListener("mouseout",function () {
-    title.textContent="Наведіть курсор"
+    title.innerHTML="Наведіть курсор"
 })
 
 let element2 = document.querySelector("button");
 
 element2.addEventListener("click",function () {
     console.warn("Якимів Данило")
-    element2.textContent="Погляньте в консоль"
+    element2.innerHTML="Погляньте в консоль"
 })
 
 element2.addEventListener("mouseout",function () {
-    element2.textContent="Натисніть кнопку"
+    element2.innerHTML="Натисніть кнопку"
 })
 
