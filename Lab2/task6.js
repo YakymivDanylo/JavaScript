5462bf86676a70009c06143920db3e93373cd3b9
YakymivDanylo <dr.yakimiv@gmail.com>
2025-02-23 11:47:20 +0200

    task 6 done

---
+++ Lab2/task6.js
@@ -0,0 +1,17 @@
+let month = prompt("Введіть місяць року", "Грудень");
+
+let season = function (month) {
+    if (month === "Грудень" || month === "Січень"  || month === "Лютий"){
+        return "Зима";
+    } else if (month === "Березень" || month === "Квітень" || month === "Травень"){
+        return "Весна";
+    } else if (month === "Червень" || month === "Липень" || month === "Серпень"){
+        return "Літо";
+    }else if (month === "Вересень" || month === "Жовтень" || month === "Листопад"){
+        return "Осінь";
+    }else {
+        return "Місяць року не розпізнано";
+    }
+}
+
+alert(season(month));
